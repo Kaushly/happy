f8b83da5893f78865a28894c0c7dc7f9697acc21
Каушлы Сергей <Kaushly@yandex.ru>
2021-02-12 01:18:44 +0300

    Можно создавать заказ на человека

---
+++ src/main/java/ru/geekbrains/happy/market/controllers/OrderController.java
@@ -0,0 +1,40 @@
+package ru.geekbrains.happy.market.controllers;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import ru.geekbrains.happy.market.dto.OrderDto;
+import ru.geekbrains.happy.market.exceptions_handling.ResourceNotFoundException;
+import ru.geekbrains.happy.market.model.User;
+import ru.geekbrains.happy.market.services.OrderService;
+import ru.geekbrains.happy.market.services.UserService;
+
+import java.security.Principal;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/v1/orders")
+@RequiredArgsConstructor
+@Slf4j
+public class OrderController {
+    private final OrderService orderService;
+    private final UserService userService;
+
+    @GetMapping("/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createOrderFromCart(Principal principal){
+        User user = userService.findByUsername(principal.getName()).orElseThrow(() -> new ResourceNotFoundException("User not found"));
+        orderService.createFromUserCart(user);
+    }
+
+    @GetMapping
+    public List<OrderDto> getCurrentUserOrders(Principal principal) {
+        return orderService.findAllOrdersByOwnerName(principal.getName()).stream().map(OrderDto::new).collect(Collectors.toList());
+    }
+
+}
--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,9 +1,10 @@
-angular.module('app', []).controller('indexController', function ($scope, $http) {
-    const contextPath = 'http://localhost:8189/happy/api/v1';
+angular.module('app', ['ngStorage']).controller('indexController', function ($scope, $http, $localStorage) {
+    const contextPath = 'http://localhost:8189/happy';
+    $scope.authorized = false;
 
     $scope.fillTable = function (pageIndex = 1) {
         $http({
-            url: contextPath + '/products',
+            url: contextPath + '/api/v1/products',
             method: 'GET',
             params: {
                 title: $scope.filter ? $scope.filter.title : null,
@@ -30,14 +31,23 @@ angular.module('app', []).controller('indexController', function ($scope, $http)
 
     $scope.showCart = function () {
         $http({
-            url: contextPath + '/cart',
+            url: contextPath + '/api/v1/cart',
             method: 'GET'
         }).then(function (response) {
             $scope.Cart = response.data;
         });
     };
 
-    $scope.generatePagesIndexes = function(startPage, endPage) {
+    $scope.showMyOrders = function () {
+        $http({
+            url: contextPath + '/api/v1/orders',
+            method: 'GET'
+        }).then(function (response) {
+            $scope.MyOrders = response.data;
+        });
+    };
+
+    $scope.generatePagesIndexes = function (startPage, endPage) {
         let arr = [];
         for (let i = startPage; i < endPage + 1; i++) {
             arr.push(i);
@@ -46,7 +56,7 @@ angular.module('app', []).controller('indexController', function ($scope, $http)
     }
 
     $scope.submitCreateNewProduct = function () {
-        $http.post(contextPath + '/products', $scope.newProduct)
+        $http.post(contextPath + '/api/v1/products', $scope.newProduct)
             .then(function (response) {
                 $scope.newProduct = null;
                 $scope.fillTable();
@@ -54,14 +64,14 @@ angular.module('app', []).controller('indexController', function ($scope, $http)
     };
 
     $scope.deleteProductById = function (productId) {
-        $http.delete(contextPath + '/products/' + productId)
+        $http.delete(contextPath + '/api/v1/products' + productId)
             .then(function (response) {
                 $scope.fillTable();
             });
     }
 
     $scope.addToCart = function (productId) {
-        $http.get(contextPath + '/cart/add/' + productId)
+        $http.get(contextPath + '/api/v1/cart/add/' + productId)
             .then(function (response) {
                 $scope.showCart();
             });
@@ -69,27 +79,53 @@ angular.module('app', []).controller('indexController', function ($scope, $http)
 
     $scope.enlarge = function (productTitle) {
         $http({
-            url: contextPath + '/cart/enlarge/' + productTitle,
+            url: contextPath + '/api/v1/cart/enlarge/' + productTitle,
             method: 'GET',
         }).then(function (response) {
             $scope.showCart()
         })
     };
 
-    $scope.reduce = function(productTitle){
-        $http.get(contextPath + '/cart/reduce/' + productTitle)
+    $scope.reduce = function (productTitle) {
+        $http.get(contextPath + '/api/v1/cart/reduce/' + productTitle)
             .then(function (response) {
                 $scope.showCart()
             });
     }
 
     $scope.clearCart = function () {
-        $http.get(contextPath + '/cart/clear')
+        $http.get(contextPath + '/api/v1/cart/clear')
             .then(function (response) {
                 $scope.showCart();
             });
     }
 
-    $scope.fillTable();
-    $scope.showCart();
+    $scope.tryToAuth = function () {
+        $http.post(contextPath + '/auth', $scope.user)
+            .then(function successCallback(response) {
+                    if (response.data.token) {
+                        $http.defaults.headers.common.Authorization = 'Bearer ' + response.data.token;
+                        $scope.user.username = null;
+                        $scope.user.password = null;
+                        $scope.authorized = true;
+                        $scope.fillTable();
+                        $scope.showMyOrders();
+                        $scope.showCart();
+                    }
+                },
+                function errorCallback(response) {
+                    window.alert("Error");
+                });
+    };
+
+    $scope.createOrder = function () {
+        $http.get(contextPath + '/api/v1/orders/create')
+            .then(function (response) {
+                $scope.showMyOrders();
+                $scope.showCart();
+            });
+    }
+
+    // $scope.fillTable();
+    // $scope.showCart();
 });
